b9f2421470f5f96dd4fb22a63d72f54daeffc005
Walter Vergara <walterwrv290888@gmail.com>
2025-03-18 09:52:27 -0300

    tp-sprint3 en proceso

---
+++ tp-sprint3/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+    
+      
+  
+  return (
+    <>
+      <p className="bg-red-500">Sprint3 en Proceso...</p>
+    </>
+  )
+}
+
+export default App
