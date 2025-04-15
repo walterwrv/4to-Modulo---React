4f49a7478a2909a661a88ff40aba9580e0ef33f4
walterwrv <walterwrv290888@gmail.com>
2025-04-14 21:10:22 -0300

    TP Sprint 5

---
+++ tp-sprint5/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='bg-red-700'>TP5 en Proceso...</p>
+    </>
+  );
+}
+
+export default App;
